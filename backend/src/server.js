758370a609b07840d70fb9797690dd6c1c7634f5
Sumit Gupta <cricsumit24@gmail.com>
2025-11-09 12:26:26 +0530

    Refactor server.js structure and add package.json for project setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 
-const app = express()
-
 dotenv.config()
 
-const PORT = process.env.PORT || 3000; 
+const app = express()
+
+const __dirname = path.resolve()
+
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
